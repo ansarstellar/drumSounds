f29912e4c9f49c7937f53cc0e4dfc2eb69f76938
Ansar <151365497+ansarstellar@users.noreply.github.com>
2024-01-17 19:42:38 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+// FIRST SOLUTION
+
+// var allButtons = document.querySelectorAll("button");
+
+// allButtons.forEach(function(button) {
+//     // Do something with each button, e.g., add an event listener
+//     button.addEventListener("click", function() {
+//         alert("Button clicked!");
+//     });
+// });
+
+
+// SECOND SOLUTION
+
+var numberOfButtons = document.querySelectorAll('.drum').length;
+
+for(var i=0; i<numberOfButtons; i++) {
+    document.querySelectorAll('.drum')[i].addEventListener('click', function () {
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+    });
+}
+
+// var audio = new Audio('./sounds/tom-1.mp3');
+//     audio.play();
+
+
+document.addEventListener('keydown', function(event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+    switch(key) {
+        case 'w':
+            var tom1 = new Audio('./sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case 'a':
+            var tom2 = new Audio('./sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case 's':
+            var tom3 = new Audio('./sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case 'd':
+            var tom4 = new Audio('./sounds/tom-4.mp3');
+            tom4.play();
+            break;
+        case 'j':
+            var crash = new Audio('./sounds/crash.mp3');
+            crash.play();
+            break;
+        case 'k':
+            var snare = new Audio('./sounds/snare.mp3');
+            snare.play();
+            break;
+        case 'l':
+            var kick = new Audio('./sounds/kick-bass.mp3');
+            kick.play();
+            break;
+    }
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector('.' + currentKey);
+    activeButton.classList.add('pressed');
+    setTimeout(function () {
+        activeButton.classList.remove('pressed');
+    }, 100);
+}
